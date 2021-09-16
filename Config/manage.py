b3b43de1592efafc6639a9613dc26b1dfa41a5b3
SEIM <68439871+SEIM-CHAN@users.noreply.github.com>
2021-09-16 12:21:06 +0900

    django start project

--- Config/manage.py
+++ Config/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Schedule_Management.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- Schedule_Management/Schedule_Management_App/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ScheduleManagementAppConfig(AppConfig):
-    name = 'Schedule_Management_App'
